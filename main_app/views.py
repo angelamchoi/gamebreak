ffa32b927476289cf60c0fcf34b6ad7de5d944d7
Angela Choi <angelamchoi@gmail.com>
2021-06-24 16:35:55 -0400

    successfully added sign up and login

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
