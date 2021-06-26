5476ed33afb0b9b822cb76f2f415669ec5e4980d
Angela Choi <angelamchoi@gmail.com>
2021-06-26 12:56:53 -0400

    added systems model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Constants
+PLAYERS = (
+    ('1', 'One'),
+    ('2', 'Two'),
+    ('3', 'Three'),
+    ('4+', 'Four')
+)
+
+# System model
+class System(models.Model):
+    name = models.CharField(max_length =250)
+    release_date= models.DateField()
+    num_players= models.IntegerField(
+        choices=PLAYERS,
+        default=PLAYERS[0][0]
+    )
+    gaming_platform = models.CharField(max_length=250)
