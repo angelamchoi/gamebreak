efaca2fea69be841bc9222e5be8fcfa4d4678a90
Angela Choi <angelamchoi@gmail.com>
2021-07-07 18:03:43 -0400

    edited views

--- main_app/views.py
+++ main_app/views.py
@@ -18,10 +18,10 @@ from .forms import GameForm
 S3_BASE_URL = 'https://s3.us-west-1.amazonaws.com/'
 BUCKET = 'gamebreak'
 
-import os
+# import os
 
-def some_function(request):
-    my_key = os.environ['SECRET_KEY']
+# def some_function(request):
+#     my_key = os.environ['SECRET_KEY']
 
 # SIGN IN/UP
 def signup(request):
